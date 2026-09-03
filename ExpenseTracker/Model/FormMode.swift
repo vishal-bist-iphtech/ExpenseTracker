cb2c03098efc66068c9c38ea32b41a6dcd774072
Vishal Bist <vishal.bist@iphtechnologies.com>
2026-09-03 18:10:32 +0530

    Added - transaction add/edit mode

---
+++ ExpenseTracker/Model/FormMode.swift
@@ -0,0 +1,14 @@
+//
+//  FormMode.swift
+//  ExpenseTracker
+//
+//  Created by iPHTech 34 on 03/09/26.
+//
+
+import Foundation
+
+enum FormMode {
+    
+    case add
+    case edit(Transaction)
+}
--- ExpenseTracker/Model/Transaction.swift
+++ ExpenseTracker/Model/Transaction.swift
@@ -17,16 +17,60 @@ struct Transaction {
     var date: Date
 }
 
-enum Category {
+enum Category: Equatable {
     case food
     case shopping
     case travel
     case bills
     case salary
     case other
+    
+    var displayName: String {
+        switch self {
+            
+        case .food: return "Food"
+        case .shopping: return "Shopping"
+        case .travel: return "Travel"
+        case .bills: return "Bills"
+        case .salary: return "Salary"
+        case .other: return "Other"
+            
+        }
+    }
+    
+    var iconName: String {
+        
+        switch self {
+            
+        case .food: return "fork.knife"
+        case .shopping: return "bag"
+        case .travel: return "airplane"
+        case .bills: return "doc.text"
+        case .salary: return "banknote"
+        case .other: return "ellipsis.circle"
+            
+        }
+    }
 }
 
-enum TransactionType {
+enum TransactionType: Equatable {
     case income
     case expense
+    
+    var displayName: String {
+        switch self {
+            
+        case .income: return "Income"
+        case .expense: return "Expense"
+            
+        }
+    }
+    
+    var sign: String {
+        switch self {
+            
+        case .income: return "+"
+        case .expense: return "-"
+        }
+    }
 }
